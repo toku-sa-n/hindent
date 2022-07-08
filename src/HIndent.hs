cfc4fca705f91f9bc544b0a53d9301ba54bf1c35
Hiroki Tokunaga <tokusan441@gmail.com>
2022-07-08 18:03:12 +0900

    Import with a qualify

--- src/HIndent.hs
+++ src/HIndent.hs
@@ -48,7 +48,6 @@ import           HIndent.Types
 import qualified Language.Haskell.Exts as Exts
 import           Language.Haskell.Exts hiding (Style, prettyPrint, Pretty, style, parse)
 import           Prelude
-import           SwitchToGhcLibParserHelper (cabalExtensionToHSEExtension)
 import qualified SwitchToGhcLibParserHelper as Helper
 
 -- | Format the given source.
@@ -70,13 +69,13 @@ reformat config mexts mfilepath =
                        Just (Nothing, exts') ->
                          mode' { extensions =
                                    exts'
-                                   ++ fmap cabalExtensionToHSEExtension (configExtensions config)
+                                   ++ fmap Helper.cabalExtensionToHSEExtension (configExtensions config)
                                    ++ extensions mode' }
                        Just (Just lang, exts') ->
                          mode' { baseLanguage = lang
                                , extensions =
                                    exts'
-                                   ++ fmap cabalExtensionToHSEExtension (configExtensions config)
+                                   ++ fmap Helper.cabalExtensionToHSEExtension (configExtensions config)
                                    ++ extensions mode' }
         in case parseModuleWithComments mode'' (UTF8.toString code) of
                ParseOk (m, comments) ->
@@ -258,10 +257,10 @@ getExtensions :: [Text] -> [Extension]
 getExtensions = foldl f defaultExtensions . map T.unpack
   where f _ "Haskell98" = []
         f a ('N':'o':x)
-          | Just x' <- fmap cabalExtensionToHSEExtension (readExtension x) =
+          | Just x' <- fmap Helper.cabalExtensionToHSEExtension (readExtension x) =
             delete x' a
         f a x
-          | Just x' <- fmap cabalExtensionToHSEExtension (readExtension x) =
+          | Just x' <- fmap Helper.cabalExtensionToHSEExtension (readExtension x) =
             x' :
             delete x' a
         f _ x = error $ "Unknown extension: " ++ x
