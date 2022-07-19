dbf86f081a5e26670003589ad63a59ef5ca7c16a
Hiroki Tokunaga <tokusan441@gmail.com>
2022-07-19 23:54:55 +0900

    Add a case statement

--- src/SwitchToGhcLibParserHelper/ConvertModule.hs
+++ src/SwitchToGhcLibParserHelper/ConvertModule.hs
@@ -9,6 +9,7 @@ module SwitchToGhcLibParserHelper.ConvertModule
   ) where
 
 import qualified GHC.Hs                as GLP
+import qualified GHC.Types.SrcLoc      as GLP
 import qualified Language.Haskell.Exts as HSE
 
 convertModule :: GLP.HsModule -> HSE.Module HSE.SrcSpanInfo
@@ -18,4 +19,19 @@ decls :: GLP.HsModule -> [HSE.Decl HSE.SrcSpanInfo]
 decls = fmap convertDecl . GLP.hsmodDecls
 
 convertDecl :: GLP.LHsDecl GLP.GhcPs -> HSE.Decl HSE.SrcSpanInfo
-convertDecl = undefined
+convertDecl (GLP.L (GLP.SrcSpanAnn _ loc) decl) =
+  case decl of
+    GLP.TyClD _ _      -> undefined
+    GLP.InstD _ _      -> undefined
+    GLP.DerivD _ _     -> undefined
+    GLP.ValD _ _       -> undefined
+    GLP.SigD _ _       -> undefined
+    GLP.KindSigD _ _   -> undefined
+    GLP.DefD _ _       -> undefined
+    GLP.ForD _ _       -> undefined
+    GLP.WarningD _ _   -> undefined
+    GLP.AnnD _ _       -> undefined
+    GLP.RuleD _ _      -> undefined
+    GLP.SpliceD _ _    -> undefined
+    GLP.DocD _ _       -> undefined
+    GLP.RoleAnnotD _ _ -> undefined
