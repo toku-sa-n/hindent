9e2787fe59e2592a28f0203e777eaec5c0337969
Hiroki Tokunaga <tokusan441@gmail.com>
2022-07-20 17:33:17 +0900

    Split a file

--- src/HIndent/Pretty.hs
+++ src/HIndent/Pretty.hs
@@ -9,15 +9,13 @@ module HIndent.Pretty
   ( pretty
   ) where
 
-import           Control.Monad.State.Strict                          hiding
-                                                                     (state)
-import qualified Data.ByteString.Builder                             as S
 import           Data.Generics.Schemes
 import           Data.Maybe                                          (mapMaybe)
 import           GHC.Driver.Ppr                                      (showPpr)
 import           GHC.Driver.Session
 import           GHC.Hs
 import           GHC.Utils.Outputable                                (Outputable)
+import           HIndent.Pretty.Combinators
 import           HIndent.Types
 import           Language.Haskell.GhclibParserEx.GHC.Settings.Config
 import           Text.Regex.TDFA
@@ -27,48 +25,6 @@ pretty :: HsModule -> Printer ()
 pretty m = do
   printPragmasToPrinter m
   printOutputableToPrinter m
-
-write :: String -> Printer ()
-write x = do
-  eol <- gets psEolComment
-  hardFail <- gets psFitOnOneLine
-  let addingNewline = eol && x /= "\n"
-  when addingNewline newline
-  state <- get
-  let writingNewline = x == "\n"
-      out :: String
-      out =
-        if psNewline state && not writingNewline
-          then replicate (fromIntegral (psIndentLevel state)) ' ' <> x
-          else x
-      psColumn' =
-        if additionalLines > 0
-          then fromIntegral (length (concat (take 1 (reverse srclines))))
-          else psColumn state + fromIntegral (length out)
-  when
-    hardFail
-    (guard
-       (additionalLines == 0 && psColumn' <= configMaxColumns (psConfig state)))
-  modify
-    (\s ->
-       s
-         { psOutput = psOutput state <> S.stringUtf8 out
-         , psNewline = False
-         , psLine = psLine state + fromIntegral additionalLines
-         , psEolComment = False
-         , psColumn = psColumn'
-         })
-  where
-    srclines = lines x
-    additionalLines = length (filter (== '\n') x)
-
-string :: String -> Printer ()
-string = write
-
-newline :: Printer ()
-newline = do
-  write "\n"
-  modify (\s -> s {psNewline = True})
 
 dynFlags :: DynFlags
 dynFlags = defaultDynFlags fakeSettings fakeLlvmConfig
--- src/HIndent/Pretty/Combinators.hs
+++ src/HIndent/Pretty/Combinators.hs
@@ -1,3 +1,48 @@
 module HIndent.Pretty.Combinators
-  (
+  ( string
+  , newline
   ) where
+
+import           Control.Monad
+import           Control.Monad.RWS       hiding (state)
+import qualified Data.ByteString.Builder as S
+import           HIndent.Types
+
+string :: String -> Printer ()
+string x = do
+  eol <- gets psEolComment
+  hardFail <- gets psFitOnOneLine
+  let addingNewline = eol && x /= "\n"
+  when addingNewline newline
+  state <- get
+  let writingNewline = x == "\n"
+      out :: String
+      out =
+        if psNewline state && not writingNewline
+          then replicate (fromIntegral (psIndentLevel state)) ' ' <> x
+          else x
+      psColumn' =
+        if additionalLines > 0
+          then fromIntegral (length (concat (take 1 (reverse srclines))))
+          else psColumn state + fromIntegral (length out)
+  when
+    hardFail
+    (guard
+       (additionalLines == 0 && psColumn' <= configMaxColumns (psConfig state)))
+  modify
+    (\s ->
+       s
+         { psOutput = psOutput state <> S.stringUtf8 out
+         , psNewline = False
+         , psLine = psLine state + fromIntegral additionalLines
+         , psEolComment = False
+         , psColumn = psColumn'
+         })
+  where
+    srclines = lines x
+    additionalLines = length (filter (== '\n') x)
+
+newline :: Printer ()
+newline = do
+  string "\n"
+  modify (\s -> s {psNewline = True})
