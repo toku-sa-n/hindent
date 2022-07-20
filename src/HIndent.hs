6bfccbac5d1f1c0e9e7877dff256a75119e9eca3
Hiroki Tokunaga <tokusan441@gmail.com>
2022-07-20 10:51:00 +0900

    Use guards

--- src/HIndent.hs
+++ src/HIndent.hs
@@ -142,17 +142,17 @@ reformat config mexts mfilepath =
        in if all (startsWithChar first) ps
             then S8.cons first (findSmallestPrefix (S.tail p : map S.tail ps))
             else ""
-    preserveTrailingNewline f x =
-      if S8.null x || S8.all isSpace x
-        then return mempty
-        else if hasTrailingLine x || configTrailingNewline config
-               then fmap
-                      (\x' ->
-                         if hasTrailingLine (L.toStrict (S.toLazyByteString x'))
-                           then x'
-                           else x' <> "\n")
-                      (f x)
-               else f x
+    preserveTrailingNewline f x
+      | S8.null x || S8.all isSpace x = return mempty
+      | otherwise =
+        if hasTrailingLine x || configTrailingNewline config
+          then fmap
+                 (\x' ->
+                    if hasTrailingLine (L.toStrict (S.toLazyByteString x'))
+                      then x'
+                      else x' <> "\n")
+                 (f x)
+          else f x
 
 -- | Does the strict bytestring have a trailing newline?
 hasTrailingLine :: ByteString -> Bool
