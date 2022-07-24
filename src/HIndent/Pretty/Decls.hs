34f2a91d8712155f287da23038de430c7876916d
Hiroki Tokunaga <tokusan441@gmail.com>
2022-07-24 10:40:13 +0900

    Use separators properly

--- src/HIndent/Pretty/Decls.hs
+++ src/HIndent/Pretty/Decls.hs
@@ -3,12 +3,25 @@ module HIndent.Pretty.Decls
   , declsExist
   ) where
 
+import           Data.Maybe
 import           GHC.Hs
+import           GHC.Types.SrcLoc
 import           HIndent.Pretty.Combinators
 import           HIndent.Types
 
 outputDecls :: HsModule -> Printer ()
-outputDecls = inter newline . fmap outputOutputable . hsmodDecls
+outputDecls =
+  mapM_ (\(x, sp) -> outputOutputable x >> fromMaybe (return ()) sp) .
+  addSeparator . fmap unLoc . hsmodDecls
 
 declsExist :: HsModule -> Bool
 declsExist = not . null . hsmodDecls
+
+addSeparator :: [HsDecl GhcPs] -> [(HsDecl GhcPs, Maybe (Printer ()))]
+addSeparator []     = []
+addSeparator [x]    = [(x, Nothing)]
+addSeparator (x:xs) = (x, Just $ separator x) : addSeparator xs
+
+separator :: HsDecl GhcPs -> Printer ()
+separator SigD {} = newline
+separator _       = blankline
