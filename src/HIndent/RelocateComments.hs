53532836440b3fb6e932c1e9848c75c03f2b50af
Hiroki Tokunaga <tokusan441@gmail.com>
2022-08-08 09:12:03 +0900

    Define `apply`

--- src/HIndent/RelocateComments.hs
+++ src/HIndent/RelocateComments.hs
@@ -1,8 +1,15 @@
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
+
 module HIndent.RelocateComments
   ( relocateComments
   ) where
 
+import           Generics.SYB    hiding (typeRep)
 import           GHC.Hs
+import           Type.Reflection
 
 -- | This function collects all comments from the passed 'HsModule', and modifies all 'EpAnn's so that all 'EpAnn's have 'EpaCommentsBalanced's.
 --
@@ -32,4 +39,19 @@ import           GHC.Hs
 --
 -- This function solves the problem by collecting all 'LEpaComment's with 'listify', and iterates all nodes from top to bottom a few times. During the first iteration, this function adds comments above each node from the collected ones to the node. On the next iteration, it adds a comment on the same line. On the last iteration, it adds comments below them.
 relocateComments :: HsModule -> HsModule
-relocateComments = undefined
+relocateComments = everywhere (apply id)
+  where
+    allComments = listify (const True)
+
+-- | This function applies the given function to all 'EpAnn's.
+apply ::
+     forall b. (Typeable b)
+  => (forall a. EpAnn a -> EpAnn a)
+  -> (b -> b)
+apply f =
+  case typeRep @b of
+    App g _ ->
+      case eqTypeRep g (typeRep @EpAnn) of
+        Just HRefl -> f
+        Nothing    -> id
+    _ -> id
