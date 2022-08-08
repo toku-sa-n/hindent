a64f023ae8f54853a5aa04bf14b7f4480083fc2f
Hiroki Tokunaga <tokusan441@gmail.com>
2022-08-08 09:30:26 +0900

    Make the code compile

--- src/HIndent/RelocateComments.hs
+++ src/HIndent/RelocateComments.hs
@@ -7,7 +7,8 @@ module HIndent.RelocateComments
   ( relocateComments
   ) where
 
-import           Generics.SYB    hiding (typeRep)
+import           Control.Monad.State
+import           Generics.SYB        hiding (typeRep)
 import           GHC.Hs
 import           Type.Reflection
 
@@ -39,19 +40,38 @@ import           Type.Reflection
 --
 -- This function solves the problem by collecting all 'LEpaComment's with 'listify', and iterates all nodes from top to bottom a few times. During the first iteration, this function adds comments above each node from the collected ones to the node. On the next iteration, it adds a comment on the same line. On the last iteration, it adds comments below them.
 relocateComments :: HsModule -> HsModule
-relocateComments = everywhere (apply id)
+relocateComments m = evalState (st m) allComments
   where
-    allComments = listify (const True)
+    st =
+      relocateCommentsBefore >=>
+      relocateCommentsSameLine >=> relocateCommentsAfter
+    allComments = listify (const True) m
 
 -- | This function applies the given function to all 'EpAnn's.
-apply ::
-     forall a. (Typeable a)
-  => (forall b. EpAnn b -> EpAnn b)
-  -> (a -> a)
-apply f =
+applyM ::
+     forall a m. (Monad m, Typeable a)
+  => (forall b. EpAnn b -> m (EpAnn b))
+  -> (a -> m a)
+applyM f =
   case typeRep @a of
     App g _ ->
       case eqTypeRep g (typeRep @EpAnn) of
         Just HRefl -> f
-        Nothing    -> id
-    _ -> id
+        Nothing    -> pure
+    _ -> pure
+
+-- | This function scans the given AST from top to bottom and locates
+-- comments in the comment pool before each node on it.
+relocateCommentsBefore :: HsModule -> State [LEpaComment] HsModule
+relocateCommentsBefore = pure
+
+-- | This function scans the given AST from top to bottom and locates
+-- comments in the comment pool above each node on it. Comments are
+-- stored in the 'followingComments' of 'EpaCommentsBalanced'.
+relocateCommentsSameLine :: HsModule -> State [LEpaComment] HsModule
+relocateCommentsSameLine = pure
+
+-- | This function scans the given AST from bottom to top and locates
+-- comments in the comment pool after each node on it.
+relocateCommentsAfter :: HsModule -> State [LEpaComment] HsModule
+relocateCommentsAfter = pure
