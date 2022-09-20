63db460477259b9f36db58b665da0e03d576d4ff
Hiroki Tokunaga <tokusan441@gmail.com>
2022-09-20 19:54:34 +0900

    Do not print the build log to the stdout

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-cabal build > /dev/null; for f in src/**/*.hs
+for f in src/**/*.hs
 do
     echo -n "$f ..."
     hindent -- $f > /dev/null
-    if ! cabal run hindent -- --validate $f
+    if ! cabal build > /dev/null && cabal run hindent -- --validate $f
     then
         cabal run hindent < $f | diff --unified=0 $f -
         break
