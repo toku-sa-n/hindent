6cb8acec01047ed7308c2962919f6477ee6cdb6c
Hiroki Tokunaga <tokusan441@gmail.com>
2022-09-25 08:45:51 +0900

    Add documentations

--- src/HIndent/Extension.hs
+++ src/HIndent/Extension.hs
@@ -8,11 +8,17 @@ import qualified HIndent.Extension.Conversion as EC
 import           HIndent.Read
 import qualified Language.Haskell.Extension   as Cabal
 
+-- | This function returns a list of extensions that the passed language
+-- (e.g., GHC2021) enables.
 implicitExtensions :: GLP.Language -> [Cabal.Extension]
 implicitExtensions =
   fmap (Cabal.EnableExtension . readOrFail . show) .
   GLP.languageExtensions . Just
 
+-- | This function returns a list of extensions that the passed extension
+-- enables.
+--
+-- For example, "GADTs" enables "GADTSyntax".
 extensionImplies :: Cabal.Extension -> [Cabal.Extension]
 extensionImplies (Cabal.EnableExtension e) =
   toExtension <$>
