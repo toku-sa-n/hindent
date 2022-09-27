8034a97177c20a5b85137c8d966e55628fa8e6ff
Hiroki Tokunaga <tokusan441@gmail.com>
2022-09-27 11:48:04 +0900

    Add a documentation

--- src/HIndent/Pretty/Pragma.hs
+++ src/HIndent/Pretty/Pragma.hs
@@ -22,6 +22,8 @@ prettyPragmas = lined . fmap string . collectPragmas
 pragmaExists :: HsModule -> Bool
 pragmaExists = not . null . collectPragmas
 
+-- | This function collects pragma comments and modifies them into
+-- 'String's.
 collectPragmas :: HsModule -> [String]
 collectPragmas =
   fmap (\x -> "{-# LANGUAGE " ++ x ++ " #-}") .
