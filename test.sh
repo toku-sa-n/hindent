e82aa094e265159aa481119bd0bd81dc2c2dc86f
Hiroki Tokunaga <tokusan441@gmail.com>
2022-09-28 17:44:32 +0900

    Fix a test script

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ do
     cp $f $TMPFILE
     echo -n "$f ..."
     hindent -- $TMPFILE > /dev/null
-    if ! cabal build > /dev/null && cabal run hindent -- --validate $TMPFILE
+    if ! (cabal build > /dev/null && cabal run hindent -- --validate $TMPFILE)
     then
         cabal run hindent < $TMPFILE | diff --unified=0 $TMPFILE -
         break
