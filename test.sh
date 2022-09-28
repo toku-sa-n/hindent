281fa1201ed4a8c8cbcd5707653a95eeea383f87
Hiroki Tokunaga <tokusan441@gmail.com>
2022-09-28 14:48:02 +0900

    Prevent the test script from modifying the original file

--- test.sh
+++ test.sh
@@ -2,11 +2,13 @@
 
 for f in src/**/*.hs
 do
+    TMPFILE=$(mktemp --suffix=.hs)
+    cp $f $TMPFILE
     echo -n "$f ..."
-    hindent -- $f > /dev/null
-    if ! cabal build > /dev/null && cabal run hindent -- --validate $f
+    hindent -- $TMPFILE > /dev/null
+    if ! cabal build > /dev/null && cabal run hindent -- --validate $TMPFILE
     then
-        cabal run hindent < $f | diff --unified=0 $f -
+        cabal run hindent < $TMPFILE | diff --unified=0 $TMPFILE -
         break
     fi
     echo " OK."
