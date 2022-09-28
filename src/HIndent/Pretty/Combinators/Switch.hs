0ab7c1ce74ab123e1a625a8a89ed5cac31a7a2eb
Hiroki Tokunaga <tokusan441@gmail.com>
2022-09-28 09:07:04 +0900

    Add a documentation

--- src/HIndent/Pretty/Combinators/Switch.hs
+++ src/HIndent/Pretty/Combinators/Switch.hs
@@ -9,6 +9,8 @@ import           Control.Applicative
 import           Control.Monad.State
 import           HIndent.Types
 
+-- | This function runs the first printer if the result of running it fits
+-- in a single line. Otherwise, it runs the second printer.
 (<-|>) :: Printer a -> Printer a -> Printer a
 fit <-|> notFit = do
   before <- get
