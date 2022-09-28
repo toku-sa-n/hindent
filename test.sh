f1f8bd06d2476ab0266db3356b48a6dde43359a1
Hiroki Tokunaga <tokusan441@gmail.com>
2022-09-28 17:58:14 +0900

    Do not output unnecessary things

--- test.sh
+++ test.sh
@@ -6,8 +6,9 @@ do
     cp $f $TMPFILE
     echo -n "$f ..."
     hindent -- $TMPFILE > /dev/null
-    if ! (cabal build > /dev/null && cabal run hindent -- --validate $TMPFILE)
+    if ! (cabal build && cabal run hindent -- --validate $TMPFILE) > /dev/null
     then
+        echo "$f is not formatted."
         cabal run hindent < $TMPFILE | diff --unified=0 $TMPFILE -
         break
     fi
