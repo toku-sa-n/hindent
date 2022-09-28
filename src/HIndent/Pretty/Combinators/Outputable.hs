0b2eebd62db8970ac22a561f5096720fdc325511
Hiroki Tokunaga <tokusan441@gmail.com>
2022-09-28 12:03:47 +0900

    Write documents

--- src/HIndent/Pretty/Combinators/Outputable.hs
+++ src/HIndent/Pretty/Combinators/Outputable.hs
@@ -13,11 +13,17 @@ import           HIndent.Pretty.Combinators.String
 import           HIndent.Types
 import           Language.Haskell.GhclibParserEx.GHC.Settings.Config
 
+-- | Prints the given value using the type's 'Outputable' implementation.
+--
+-- The use of this function should be avoided if possible because it may
+-- raise an error due to 'showPpr' returning a 'String' containing @\n@s.
 output :: (HasCallStack, Outputable a) => a -> Printer ()
 output = string . showOutputable
 
+-- | Converts the given value to a 'String'.
 showOutputable :: Outputable a => a -> String
 showOutputable = showPpr dynFlags
 
+-- | 'DynFlags' for calling 'showPpr'
 dynFlags :: DynFlags
 dynFlags = defaultDynFlags fakeSettings fakeLlvmConfig
