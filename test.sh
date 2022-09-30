eb33e8f489b67911ea4e22e4c2a8c4d3dd9df646
Hiroki Tokunaga <tokusan441@gmail.com>
2022-10-01 08:18:31 +0900

    Exit a loop if necessary

--- test.sh
+++ test.sh
@@ -7,8 +7,8 @@ do
     cp $f $EXPECTED
     cp $f $ACTUAL
     echo -n "$f ..."
-    hindent -- $EXPECTED > /dev/null
-    cabal run hindent -- $ACTUAL > /dev/null
+    hindent -- $EXPECTED > /dev/null || (echo "The original HIndent failed to format the file."; break)
+    cabal run hindent -- $ACTUAL > /dev/null || break
     if ! diff $EXPECTED $ACTUAL > /dev/null
     then
         echo "$f is not formatted."
