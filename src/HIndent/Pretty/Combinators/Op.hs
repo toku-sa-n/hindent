d4f424af96154928bc354a7207ff0c75216920e9
Hiroki Tokunaga <tokusan441@gmail.com>
2022-09-30 14:53:07 +0900

    Write documents

--- src/HIndent/Pretty/Combinators/Op.hs
+++ src/HIndent/Pretty/Combinators/Op.hs
@@ -1,3 +1,4 @@
+-- | Printer combinators related to operators.
 module HIndent.Pretty.Combinators.Op
   ( unlessSpecialOp
   ) where
@@ -7,14 +8,17 @@ import           GHC.Types.Name
 import           GHC.Types.Name.Reader
 import           HIndent.Types
 
+-- | Runs the printer unless HIndent needs to treat the operator specially.
 unlessSpecialOp :: RdrName -> Printer () -> Printer ()
 unlessSpecialOp name = unless (isSpecialOp name)
 
+-- | Returns if HIndent needs special treatment for the operator.
 isSpecialOp :: RdrName -> Bool
 isSpecialOp (Unqual name) = isSpecialOpString $ occNameString name
 isSpecialOp Qual {}       = undefined
 isSpecialOp Orig {}       = undefined
 isSpecialOp (Exact name)  = isSpecialOpString $ occNameString $ nameOccName name
 
+-- | Returns if HIndent needs special treatment for the operator.
 isSpecialOpString :: String -> Bool
 isSpecialOpString name = name `elem` ["()", "[]", "->", ":"]
