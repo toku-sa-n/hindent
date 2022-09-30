fc83585e967b1d0da6ac111236ed12ebf3196500
Hiroki Tokunaga <tokusan441@gmail.com>
2022-10-01 08:17:43 +0900

    Revert "Test more files"
    This reverts commit 61ff26338be1a32fa9abaa528b909c6c5baeb7d6.

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 
-for f in ./**/*.hs
+for f in src/**/*.hs
 do
     EXPECTED=$(mktemp --suffix=.expected.hs)
     ACTUAL=$(mktemp --suffix=.actual.hs)
     cp $f $EXPECTED
     cp $f $ACTUAL
     echo -n "$f ..."
-    hindent -- $EXPECTED > /dev/null || (echo "The original HIndent failed." && continue)
-    cabal run hindent -- $ACTUAL > /dev/null || break
+    hindent -- $EXPECTED > /dev/null
+    cabal run hindent -- $ACTUAL > /dev/null
     if ! diff $EXPECTED $ACTUAL > /dev/null
     then
         echo "$f is not formatted."
