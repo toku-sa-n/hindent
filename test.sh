61ff26338be1a32fa9abaa528b909c6c5baeb7d6
Hiroki Tokunaga <tokusan441@gmail.com>
2022-09-30 23:03:13 +0900

    Test more files

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 
-for f in src/**/*.hs
+for f in ./**/*.hs
 do
     EXPECTED=$(mktemp --suffix=.expected.hs)
     ACTUAL=$(mktemp --suffix=.actual.hs)
     cp $f $EXPECTED
     cp $f $ACTUAL
     echo -n "$f ..."
-    hindent -- $EXPECTED > /dev/null
-    cabal run hindent -- $ACTUAL > /dev/null
+    hindent -- $EXPECTED > /dev/null || (echo "The original HIndent failed." && continue)
+    cabal run hindent -- $ACTUAL > /dev/null || break
     if ! diff $EXPECTED $ACTUAL > /dev/null
     then
         echo "$f is not formatted."
