eb15ddc66add6ca02614e64b585725fa5a99faad
Hiroki Tokunaga <tokusan441@gmail.com>
2022-10-02 22:22:32 +0900

    Revert "Handle `HsConLikeOut`"
    This reverts commit c993e6ff7b104403c322bc4d1ea414045a686903.
    
    I'm not sure if it actually never appears in an AST.

--- src/HIndent/Pretty.hs
+++ src/HIndent/Pretty.hs
@@ -28,7 +28,6 @@ import           GHC.Data.Bag
 import           GHC.Data.BooleanFormula
 import           GHC.Data.FastString
 import           GHC.Hs
-import           GHC.Stack
 import           GHC.Types.Fixity
 import           GHC.Types.Name
 import           GHC.Types.Name.Reader
@@ -585,11 +584,11 @@ instance Pretty (HsExpr GhcPs) where
 
 -- HIndent cannot handle CPP instructions inside a class instance
 -- declaration well. That is why this function is defined.
-prettyHsExpr :: HasCallStack => HsExpr GhcPs -> Printer ()
+prettyHsExpr :: HsExpr GhcPs -> Printer ()
 prettyHsExpr (HsVar _ bind) = pretty $ fmap PrefixOp bind
 prettyHsExpr (HsUnboundVar _ x) = pretty x
 #if !MIN_VERSION_ghc_lib_parser(9,4,1)
-prettyHsExpr HsConLikeOut {} = error "This should not appear in an AST."
+prettyHsExpr HsConLikeOut {} = undefined
 prettyHsExpr HsRecFld {} = undefined
 #endif
 prettyHsExpr (HsOverLabel _ l) = do
