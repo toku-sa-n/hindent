4f8558e19ab54e300fdf8fa9444f71494386db1b
Hiroki Tokunaga <tokusan441@gmail.com>
2022-10-02 12:30:10 +0900

    Do not print any logs

--- input_test.sh
+++ input_test.sh
@@ -5,6 +5,4 @@ readonly INPUT=$(cat -)
 readonly EXPECTED=$(echo $INPUT|hindent)
 readonly ACTUAL=$(echo $INPUT|stack run --silent)
 
-echo "Executing ..."
-
 diff <(echo "$EXPECTED") <(echo "$ACTUAL")
