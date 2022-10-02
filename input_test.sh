04d4cf9560f562f89036573d4664e0fa1623c2af
Hiroki Tokunaga <tokusan441@gmail.com>
2022-10-02 13:37:29 +0900

    Fix getting the input

--- input_test.sh
+++ input_test.sh
@@ -1,8 +1,23 @@
-#!/bin/bash
+#!/bin/zsh
 
-readonly INPUT=$(cat -)
+readonly INPUT=$(read -se)
 
-readonly EXPECTED=$(echo $INPUT|hindent)
-readonly ACTUAL=$(echo $INPUT|stack run --silent)
+readonly EXPECTED=$(mktemp --suffix=.expected.hs)
+readonly ACTUAL=$(mktemp --suffix=.actual.hs)
 
-diff <(echo "$EXPECTED") <(echo "$ACTUAL")
+echo $INPUT > $EXPECTED
+echo $INPUT > $ACTUAL
+
+if ! hindent $EXPECTED
+then
+    echo "The original HIndent failed to format the code."
+    exit 1
+fi
+
+if ! stack run --silent $ACTUAL
+then
+    echo "The development version of HIndent failed to format the code."
+    exit 1
+fi
+
+diff $EXPECTED $ACTUAL
