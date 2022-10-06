d843bf81d1012e62f45c426ab884e78656f953fb
Hiroki Tokunaga <tokusan441@gmail.com>
2022-10-06 14:06:22 +0900

    Read pragmas in a source code

--- src/HIndent/Extension.hs
+++ src/HIndent/Extension.hs
@@ -1,15 +1,21 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 -- | Operations related to language extensions.
 module HIndent.Extension
   ( implicitExtensions
   , extensionImplies
+  , collectLanguageExtensionsFromSource
   ) where
 
+import           Data.List.Split
 import           Data.Maybe
 import qualified GHC.Driver.Session           as GLP
 import           HIndent.Extension.Conversion
 import qualified HIndent.Extension.Conversion as EC
 import           HIndent.Read
 import qualified Language.Haskell.Extension   as Cabal
+import           Text.Read
+import           Text.Regex.TDFA
 
 -- | This function returns a list of extensions that the passed language
 -- (e.g., GHC2021) enables.
@@ -29,3 +35,18 @@ extensionImplies (Cabal.EnableExtension e) =
     toExtension (_, True, e')  = Cabal.EnableExtension $ readOrFail $ show e'
     toExtension (_, False, e') = Cabal.DisableExtension $ readOrFail $ show e'
 extensionImplies _ = []
+
+-- | Collects language extensions enabled or disabled by @{-# LANGUAGE FOO
+-- #-}@.
+--
+-- This function ignores language extensions not supported by Cabal.
+collectLanguageExtensionsFromSource :: String -> [Cabal.Extension]
+collectLanguageExtensionsFromSource = concatMap lineToExt . lines
+  where
+    lineToExt :: String -> [Cabal.Extension]
+    lineToExt l
+      | (_, _, _, exts) :: (String, String, String, [String]) <- l =~ regex =
+        mapMaybe strToExt $ concatMap (splitOn ",") exts
+    strToExt ('N':'o':s) = Cabal.DisableExtension <$> readMaybe s
+    strToExt s           = Cabal.EnableExtension <$> readMaybe s
+    regex = "{-# +LANGUAGE +([a-zA-Z0-9-,]+) +#-}"
