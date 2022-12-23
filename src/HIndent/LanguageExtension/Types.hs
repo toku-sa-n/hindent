4cf0354c3a5355eb125feeedc54c503c59508e65
Hiroki Tokunaga <tokusan441@gmail.com>
2022-12-23 16:21:30 +0900

    Improve the document

--- src/HIndent/LanguageExtension/Types.hs
+++ src/HIndent/LanguageExtension/Types.hs
@@ -8,7 +8,7 @@ import qualified GHC.LanguageExtensions as GLP
 -- | Language Extension. Either enabled or disabled.
 --
 -- The `Cabal` package also has an `Extension` type that can be used to
--- indicate whether an extension is enabled or disabled, but this type
+-- indicate whether an extension is enabled or disabled, but Cabal's one
 -- should be avoided as much as possible. The `KnownExtension` of `Cabal`
 -- may not have the latest extensions, and if such extensions are used,
 -- there will be cases where GHC can build, but HIndent cannot format.
