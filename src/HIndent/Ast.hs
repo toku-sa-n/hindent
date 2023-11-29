c87224aa738246d5fdefc1d0a2f405e26027ffea
Hiroki Tokunaga <tokusan441@gmail.com>
2023-11-29 15:35:37 +0900

    Define a custom `Ast`

---
+++ src/HIndent/Ast.hs
@@ -0,0 +1,13 @@
+-- | AST type and its components.
+--
+-- The Abstract Syntax Tree (AST) provided by ghc-lib-parser, specifically
+-- HsModule and its components, undergoes frequent changes with each update of
+-- the library. To accommodate the differences between multiple versions of the
+-- same library, HIndent defines its own custom AST type.
+module HIndent.Ast
+  ( Ast
+  ) where
+
+import qualified GHC.Hs as GHC
+
+type Ast = GHC.HsModule
