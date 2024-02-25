a560820bdaa56626d43a68331a2bb768dfdda80a
Hiroki Tokunaga <tokusan441@gmail.com>
2024-02-25 12:30:55 +0900

    Make hlint happy

--- src/HIndent/Ast/Module.hs
+++ src/HIndent/Ast/Module.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs            #-}
 {-# LANGUAGE LambdaCase       #-}
-{-# LANGUAGE NamedFieldPuns   #-}
 {-# LANGUAGE RecordWildCards  #-}
 {-# LANGUAGE ViewPatterns     #-}
 
