d5af40a3ddc2aaea2650d6e68942d99e3539c885
Hiroki Tokunaga <tokusan441@gmail.com>
2024-02-25 17:02:36 +0900

    Implement `Pretty` for `ExportGroup`

--- src/HIndent/Ast/Export.hs
+++ src/HIndent/Ast/Export.hs
@@ -7,9 +7,17 @@ module HIndent.Ast.Export
   ) where
 
 import GHC.Hs
+import HIndent.Pretty
+import HIndent.Pretty.NodeComments
 
 newtype Export =
   Export (IE GhcPs)
+
+instance CommentExtraction Export where
+  nodeComments (Export x) = nodeComments x
+
+instance Pretty Export where
+  pretty' (Export x) = pretty' x
 
 mkExport :: IE GhcPs -> Export
 mkExport = Export
--- src/HIndent/Ast/ExportGroup.hs
+++ src/HIndent/Ast/ExportGroup.hs
@@ -11,6 +11,10 @@ import GHC.Hs
 import GHC.Types.SrcLoc
 import HIndent.Ast.Export
 import HIndent.Ast.WithComments
+import HIndent.Pretty
+import HIndent.Pretty.Combinators
+import HIndent.Pretty.NodeComments
+import HIndent.Pretty.Types
 #if MIN_VERSION_ghc_lib_parser(9, 6, 1)
 type HsModule' = HsModule GhcPs
 #else
@@ -20,6 +24,16 @@ data ExportGroup
   = ExportAll
   | NoExports
   | ExportList (NonEmpty (WithComments Export))
+
+instance CommentExtraction ExportGroup where
+  nodeComments ExportAll = NodeComments [] [] []
+  nodeComments NoExports = NodeComments [] [] []
+  nodeComments (ExportList _) = NodeComments [] [] []
+
+instance Pretty ExportGroup where
+  pretty' ExportAll = pure ()
+  pretty' NoExports = string "()"
+  pretty' (ExportList exports) = vTuple (pretty <$> toList exports)
 
 mkExportGroup :: HsModule' -> ExportGroup
 mkExportGroup HsModule {..} =
