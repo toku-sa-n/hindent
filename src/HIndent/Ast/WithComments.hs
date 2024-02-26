fddaf2f6c8c82dd35533ce2c51a143f263c53115
Hiroki Tokunaga <tokusan441@gmail.com>
2024-02-26 15:56:17 +0900

    printCommentsAnd

--- src/HIndent/Ast/WithComments.hs
+++ src/HIndent/Ast/WithComments.hs
@@ -1,19 +1,25 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ViewPatterns #-}
 
 -- | AST with comments.
 module HIndent.Ast.WithComments
   ( WithComments
   , mkWithCommentsWithEpAnn
   , mkWithCommentsWithGenLocated
+  , printCommentsAnd
   , getNode
   ) where
 
+import Control.Monad
+import Control.Monad.RWS
 import GHC.Hs
 import GHC.Types.SrcLoc
 import HIndent.Ast.Pragma
 import HIndent.Pretty
+import HIndent.Pretty.Combinators
 import HIndent.Pretty.NodeComments
 import HIndent.Pretty.Types
+import HIndent.Printer
 
 data WithComments a = WithComments
   { comments :: NodeComments
@@ -70,3 +76,45 @@ filterOutEofAndPragmas = filter isNeitherEofNorPragmaComment
 isNeitherEofNorPragmaComment :: GenLocated l EpaComment -> Bool
 isNeitherEofNorPragmaComment (L _ (EpaComment EpaEofComment _)) = False
 isNeitherEofNorPragmaComment (L _ (EpaComment tok _)) = not $ isPragma tok
+
+printCommentsAnd :: WithComments a -> (a -> Printer ()) -> Printer ()
+printCommentsAnd x f = do
+  printCommentsBefore x
+  f $ node x
+  printCommentOnSameLine x
+  printCommentsAfter x
+
+-- | Prints comments that are before the given AST node.
+printCommentsBefore :: CommentExtraction a => a -> Printer ()
+printCommentsBefore p =
+  forM_ (commentsBefore $ nodeComments p) $ \(L loc c) -> do
+    let col = fromIntegral $ srcSpanStartCol (anchor loc) - 1
+    indentedWithFixedLevel col $ pretty c
+    newline
+
+-- | Prints comments that are on the same line as the given AST node.
+printCommentOnSameLine :: CommentExtraction a => a -> Printer ()
+printCommentOnSameLine (commentsOnSameLine . nodeComments -> (c:cs)) = do
+  col <- gets psColumn
+  if col == 0
+    then indentedWithFixedLevel
+           (fromIntegral $ srcSpanStartCol $ anchor $ getLoc c)
+           $ spaced
+           $ fmap pretty
+           $ c : cs
+    else spacePrefixed $ fmap pretty $ c : cs
+  eolCommentsArePrinted
+printCommentOnSameLine _ = return ()
+
+-- | Prints comments that are after the given AST node.
+printCommentsAfter :: CommentExtraction a => a -> Printer ()
+printCommentsAfter p =
+  case commentsAfter $ nodeComments p of
+    [] -> return ()
+    xs -> do
+      isThereCommentsOnSameLine <- gets psEolComment
+      unless isThereCommentsOnSameLine newline
+      forM_ xs $ \(L loc c) -> do
+        let col = fromIntegral $ srcSpanStartCol (anchor loc) - 1
+        indentedWithFixedLevel col $ pretty c
+        eolCommentsArePrinted
