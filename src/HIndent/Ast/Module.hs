069766d12c139b4988894d109a2555b915eeb273
Hiroki Tokunaga <tokusan441@gmail.com>
2024-02-26 13:02:18 +0900

    Removed

--- src/HIndent/Ast/Module.hs
+++ src/HIndent/Ast/Module.hs
@@ -1,46 +1,45 @@
 -- | Module type.
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE CPP              #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE GADTs            #-}
+{-# LANGUAGE NamedFieldPuns   #-}
+{-# LANGUAGE RecordWildCards  #-}
 
 module HIndent.Ast.Module
   ( Module(..)
   , mkModule
   ) where
 
-import Data.Maybe
-import GHC.Hs hiding (comments)
-import HIndent.Ast.Declaration
-import HIndent.Ast.Import
-import HIndent.Ast.Module.Declaration
-import HIndent.Ast.Pragma
-import HIndent.Ast.WithComments
-import HIndent.Pretty
-import HIndent.Pretty.Combinators
-import HIndent.Pretty.NodeComments
+import           Data.Maybe
+import           GHC.Hs                         hiding (comments)
+import           HIndent.Ast.Declaration
+import           HIndent.Ast.Import
+import           HIndent.Ast.Module.Declaration
+import           HIndent.Ast.Pragma
+import           HIndent.Ast.WithComments
+import           HIndent.Pretty
+import           HIndent.Pretty.Combinators
+import           HIndent.Pretty.NodeComments
+import           HIndent.Pretty.Types
 #if MIN_VERSION_ghc_lib_parser(9, 6, 1)
 type HsModule' = HsModule GhcPs
 #else
 type HsModule' = HsModule
 #endif
 data Module = Module
-  { pragmas :: FileHeaderPragmaCollection
-  , declaration :: Maybe ModuleDeclaration
-  , imports :: ImportCollection
+  { pragmas      :: FileHeaderPragmaCollection
+  , declaration  :: Maybe ModuleDeclaration
+  , imports      :: ImportCollection
   , declarations :: DeclarationCollection
-  , module' :: HsModule'
   }
 
 instance CommentExtraction Module where
-  nodeComments (Module {..}) = nodeComments module'
+  nodeComments (Module {}) = NodeComments [] [] []
 
 instance Pretty Module where
   pretty' Module {declaration = Nothing, pragmas, imports, declarations}
-    | not (pragmaExists pragmas)
-        && not (hasImports imports)
-        && not (hasDeclarations declarations) = pure ()
+    | not (pragmaExists pragmas) &&
+        not (hasImports imports) && not (hasDeclarations declarations) = pure ()
   pretty' Module {..} = blanklined printers >> newline
     where
       printers = snd <$> filter fst pairs
@@ -50,7 +49,7 @@ instance Pretty Module where
         , (hasImports imports, pretty imports)
         , (hasDeclarations declarations, pretty declarations)
         ]
-      prettyModuleDecl Nothing = error "The module declaration does not exist."
+      prettyModuleDecl Nothing  = error "The module declaration does not exist."
       prettyModuleDecl (Just d) = pretty d
       moduleDeclExists = isJust declaration
 
@@ -62,7 +61,6 @@ mkModule m = mkWithCommentsWithEpAnn ann Module {..}
     pragmas = mkFileHeaderPragmaCollection m
     imports = mkImportCollection m
     declarations = mkDeclarationCollection m
-    module' = m
 
 getAnn :: HsModule' -> EpAnn AnnsModule
 #if MIN_VERSION_ghc_lib_parser(9, 6, 1)
