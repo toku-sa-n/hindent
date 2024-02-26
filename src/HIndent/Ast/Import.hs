f10fcd0ace8a8fc28100eb3d9101cec362d26d3b
Hiroki Tokunaga <tokusan441@gmail.com>
2024-02-26 13:33:38 +0900

    Inline functions

--- src/HIndent/Ast/Import.hs
+++ src/HIndent/Ast/Import.hs
@@ -11,10 +11,11 @@ module HIndent.Ast.Import
 
 import Control.Monad.RWS
 import GHC.Hs
+import GHC.Types.SrcLoc
 import HIndent.Config
 import HIndent.Pretty
 import HIndent.Pretty.Combinators
-import HIndent.Pretty.Import
+import HIndent.Pretty.Import.Sort
 import HIndent.Pretty.NodeComments
 import HIndent.Pretty.Types
 import HIndent.Printer
@@ -51,3 +52,30 @@ mkImport import' = Import {isSafeImport = True, import'}
 
 hasImports :: ImportCollection -> Bool
 hasImports (ImportCollection imports) = not $ null imports
+
+extractImports' :: [LImportDecl GhcPs] -> [[LImportDecl GhcPs]]
+extractImports' = groupImports . sortImportsByLocation
+
+extractImportsSorted' :: [LImportDecl GhcPs] -> [[LImportDecl GhcPs]]
+extractImportsSorted' = fmap sortImportsByName . extractImports'
+
+-- | Combines adjacent import declarations into a single list.
+groupImports :: [LImportDecl GhcPs] -> [[LImportDecl GhcPs]]
+groupImports = groupImports' []
+  where
+    groupImports' ::
+         [[LImportDecl GhcPs]] -> [LImportDecl GhcPs] -> [[LImportDecl GhcPs]]
+    groupImports' xs [] = xs
+    groupImports' [] (x:xs) = groupImports' [[x]] xs
+    groupImports' [[]] (x:xs) = groupImports' [[x]] xs
+    groupImports' ([]:x:xs) (y:ys) = groupImports' ([y] : x : xs) ys
+    groupImports' ((z:zs):xs) (y:ys)
+      | z `isAdjacentTo` y = groupImports' ((y : z : zs) : xs) ys
+      | otherwise = groupImports' ([y] : (z : zs) : xs) ys
+    a `isAdjacentTo` b =
+      srcSpanEndLine (sp a) + 1 == srcSpanStartLine (sp b)
+        || srcSpanEndLine (sp b) + 1 == srcSpanStartLine (sp a)
+    sp x =
+      case locA $ getLoc x of
+        RealSrcSpan x' _ -> x'
+        _ -> error "Src span unavailable."
